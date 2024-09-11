e9c0e5f9a649402e2cbe38c6b5f67a455dfcd8b2
nsingla <nsingla@thesummitgrp.com>
2024-09-11 11:35:42 +0530

    main file updated

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import fasttext
 
-def main():
-    print("Hello world")
+# Path to the training data
+training_data_path = 'labeled_name_data.txt'
+
+# Train the FastText model
+model = fasttext.train_supervised(input=training_data_path, epoch=25, lr=1.0, wordNgrams=2, verbose=2, minCount=1)
+
+# Save the model for later use
+model.save_model("name_non_name_model.bin")
+
+# Evaluate the model on the same training data (or you can split your data into training and test sets)
+result = model.test(training_data_path)
+
+# Print evaluation results
+print(f"Number of examples: {result[0]}")
+print(f"Precision: {result[1]}")
+print(f"Recall: {result[2]}")
