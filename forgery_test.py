41fdaf97b932bd1a98a3a04dbdddb1fb99d0292c
nsingla <nsingla@thesummitgrp.com>
2024-09-12 17:17:09 +0530

    forgery code updated with ela

--- forgery_test.py
+++ forgery_test.py
@@ -3,10 +3,30 @@ import cv2
 from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import to_categorical
 from PIL import Image
+from PIL import Image, ImageChops, ImageEnhance
 
 # Load the pre-trained model
 model = load_model('model_casia_run1.h5')
 
+def convert_to_ela_image(path, quality):
+    temp_filename = 'temp_file_name.jpg'
+    ela_filename = 'temp_ela.png'
+
+    image = Image.open(path).convert('RGB')
+    image.save(temp_filename, 'JPEG', quality = quality)
+    temp_image = Image.open(temp_filename)
+
+    ela_image = ImageChops.difference(image, temp_image)
+
+    extrema = ela_image.getextrema()
+    max_diff = max([ex[1] for ex in extrema])
+    if max_diff == 0:
+        max_diff = 1
+    scale = 255.0 / max_diff
+
+    ela_image = ImageEnhance.Brightness(ela_image).enhance(scale)
+
+    return ela_image
 def prepare_image(image_path):
     # Convert to ELA image and preprocess the same way as in training
     ela_image = convert_to_ela_image(image_path, 91)
