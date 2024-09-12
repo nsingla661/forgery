979eada779c660d30bd584ca1895d7d33bee80e2
nsingla <nsingla@thesummitgrp.com>
2024-09-12 17:03:57 +0530

    forger test changed with images

--- forgery_test.py
+++ forgery_test.py
@@ -28,3 +28,75 @@ Y_pred_classes = np.argmax(Y_pred, axis=1)
 
 # Print the predicted class
 print(f'Predicted class: {Y_pred_classes[0]}')
+
+
+
+# Load and prepare the test image
+image_path = 'photo_2024-09-10 13.46.53.jpeg'
+image = prepare_image(image_path)
+image = image.reshape(1, 128, 128, 3)  # Add batch dimension
+
+# Predict the values
+Y_pred = model.predict(image)
+
+# Convert predictions to class labels
+Y_pred_classes = np.argmax(Y_pred, axis=1)
+
+# Print the predicted class
+print(f'Predicted class for flash on document: {Y_pred_classes[0]}')
+
+# Load and prepare the test image
+image_path = 'image (9).png'
+image = prepare_image(image_path)
+image = image.reshape(1, 128, 128, 3)  # Add batch dimension
+
+# Predict the values
+Y_pred = model.predict(image)
+
+# Convert predictions to class labels
+Y_pred_classes = np.argmax(Y_pred, axis=1)
+
+# Print the predicted class
+print(f'Predicted class for image 9 : {Y_pred_classes[0]}')
+
+# Load and prepare the test image
+image_path = 'image (10).png'
+image = prepare_image(image_path)
+image = image.reshape(1, 128, 128, 3)  # Add batch dimension
+
+# Predict the values
+Y_pred = model.predict(image)
+
+# Convert predictions to class labels
+Y_pred_classes = np.argmax(Y_pred, axis=1)
+
+# Print the predicted class
+print(f'Predicted class: {Y_pred_classes[0]}')
+
+# Load and prepare the test image
+image_path = 's1.png'
+image = prepare_image(image_path)
+image = image.reshape(1, 128, 128, 3)  # Add batch dimension
+
+# Predict the values
+Y_pred = model.predict(image)
+
+# Convert predictions to class labels
+Y_pred_classes = np.argmax(Y_pred, axis=1)
+
+# Print the predicted class
+print(f'Predicted class of slack: {Y_pred_classes[0]}')
+
+# Load and prepare the test image
+image_path = 's2.png'
+image = prepare_image(image_path)
+image = image.reshape(1, 128, 128, 3)  # Add batch dimension
+
+# Predict the values
+Y_pred = model.predict(image)
+
+# Convert predictions to class labels
+Y_pred_classes = np.argmax(Y_pred, axis=1)
+
+# Print the predicted class
+print(f'Predicted class for blank image: {Y_pred_classes[0]}')
