cc274314ea7ef2025e72c6106d1e93c8c23521c4
nsingla <nsingla@thesummitgrp.com>
2024-09-17 21:04:31 +0530

    kjnfkt

--- aws_model/aws_model_prediction.py
+++ aws_model/aws_model_prediction.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import load_model
 from PIL import Image, ImageChops, ImageEnhance
 
 # Load the trained model
-model = load_model('/home/ubuntu/forgery/forgery/aws_model/aws_model.h5')
+model = load_model('/home/ubuntu/forgery/forgery/aws_model/aws_model_4_augmented.h5')
 
 def convert_to_ela_image(path, quality=90):
     resaved_filename = 'tempresaved.jpg'
@@ -38,7 +38,7 @@ def predict_image(image_path):
     
     # Predict using the model
     prediction = model.predict(processed_image)
-    print(prediction)
+    # print(prediction)
     class_label = np.argmax(prediction, axis=1)
     
     return class_label[0]
@@ -46,15 +46,21 @@ def predict_image(image_path):
 # Paths to the folders
 camera_clicked_path = '/home/ubuntu/forgery/forgery/camera_clicked/'
 license_docs_path = '/home/ubuntu/forgery/forgery/license_docs/'
+data_au = '/home/ubuntu/forgery/forgery/data/input/casia-dataset/CASIA2/Au'
+data_tp = '/home/ubuntu/forgery/forgery/data/input/casia-dataset/CASIA2/Tp'
 
 def predict_folder(folder_path):
-    print(f"Predicting for images in folder: {folder_path}")
+    count = 0 
+    total = 0
     for file in os.listdir(folder_path):
         if file.endswith(('jpg', 'jpeg', 'png')):
             image_path = os.path.join(folder_path, file)
             class_label = predict_image(image_path)
-            print(f"Image: {file}, Predicted Class: {class_label}")
+            count += class_label
+            total += 1
+            # print(f"Image: {file}, Predicted Class: {class_label}")
+    print(f"Predicting for images in folder: {folder_path} , count : {count} , total = {total}")
 
 # Run predictions for both folders
-predict_folder(camera_clicked_path)
-predict_folder(license_docs_path)
+
+predict_folder(data_tp)
