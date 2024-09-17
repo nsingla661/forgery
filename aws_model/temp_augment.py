2812a5c1fb782d236b3e88ebf7b73188e352fb60
nsingla <nsingla@thesummitgrp.com>
2024-09-17 20:53:28 +0530

    datagen added

--- aws_model/temp_augment.py
+++ aws_model/temp_augment.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+np.random.seed(2)
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 import itertools
@@ -14,11 +16,13 @@ from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
 from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+
 from PIL import Image
 from pylab import *
 from PIL import Image, ImageChops, ImageEnhance
 
-np.random.seed(2)
+from datetime import datetime
+import cv2
 
 def convert_to_ela_image(path, quality):
     filename = path
@@ -26,8 +30,8 @@ def convert_to_ela_image(path, quality):
     im = Image.open(filename)
     bm = im.convert('RGB')
     im.close()
-    im = bm
-    im.save(resaved_filename, 'JPEG', quality=quality)
+    im=bm
+    im.save(resaved_filename, 'JPEG', quality = quality)
     resaved_im = Image.open(resaved_filename)
     ela_im = ImageChops.difference(im, resaved_im)
     extrema = ela_im.getextrema()
@@ -39,19 +43,27 @@ def convert_to_ela_image(path, quality):
     im.close()
     bm.close()
     resaved_im.close()
-    del filename, resaved_filename, im, bm, resaved_im, extrema, max_diff, scale
+    del filename
+    del resaved_filename
+    del im
+    del bm
+    del resaved_im
+    del extrema
+    del max_diff
+    del scale
     return ela_im
 
 def build_image_list(path_to_image, label, images):
     for file in tqdm(os.listdir(path_to_image)):
         try:
-            if file.endswith(('jpg', 'JPG', 'jpeg', 'JPEG')):
+            if file.endswith('jpg') or file.endswith('JPG') or file.endswith('jpeg') or file.endswith('JPEG'):
                 if int(os.stat(path_to_image + file).st_size) > 10000:
-                    line = path_to_image + file + ',' + label + '\n'
+                    line = path_to_image + file  + ',' + label + '\n'
                     images.append(line)
         except:
             print(path_to_image + file)
     return images
+    
 
 custom_path_original = 'images/training/original/'
 custom_path_tampered = 'images/training/forged/'
@@ -68,21 +80,22 @@ for i in tqdm(range(len(images))):
     image_name.append(images[i][0:-3])
     label.append(images[i][-2])
 
-dataset = pd.DataFrame({'image': image_name, 'class_label': label})
-dataset.to_csv(training_data_set, index=False)
+dataset = pd.DataFrame({'image':image_name,'class_label':label})
+dataset.to_csv(training_data_set,index=False)
 
 dataset = pd.read_csv('dataset.csv')
 X = []
 Y = []
 for index, row in dataset.iterrows():
-    X.append(np.array(convert_to_ela_image(row[0], 90).resize((128, 128))).flatten() / 255.0)
+    X.append(array(convert_to_ela_image(row[0], 90).resize((128, 128))).flatten() / 255.0)
     Y.append(row[1])
 X = np.array(X)
 Y = to_categorical(Y, 2)
 
+
 X = X.reshape(-1, 128, 128, 3)
 
-X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2, random_state=5)
+X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size = 0.2, random_state=5)
 
 # Data Augmentation
 datagen = ImageDataGenerator(
@@ -99,24 +112,29 @@ datagen.fit(X_train)
 
 model = Sequential()
 
-model.add(Conv2D(filters=32, kernel_size=(3, 3), padding='valid', activation='relu', input_shape=(128, 128, 3)))
-model.add(Conv2D(filters=32, kernel_size=(3, 3), padding='valid', activation='relu'))
-model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Conv2D(filters = 32, kernel_size = (3,3),padding = 'valid', 
+                 activation ='relu', input_shape = (128,128,3)))
+model.add(Conv2D(filters = 32, kernel_size = (3,3),padding = 'valid', 
+                 activation ='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(256, activation='relu'))
+model.add(Dense(256, activation = "relu"))
 model.add(Dropout(0.5))
-model.add(Dense(2, activation='softmax'))
+model.add(Dense(2, activation = "softmax"))
 
 model.summary()
 
 optimizer = RMSprop(learning_rate=0.0005, rho=0.9, epsilon=1e-08, decay=0.0)
-model.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy"])
+model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
 
 epochs = 20
 batch_size = 100
 
-early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto')
+early_stopping = EarlyStopping(monitor='val_loss',
+                              min_delta=0,
+                              patience=5,
+                              verbose=0, mode='auto')
 
 history = model.fit(datagen.flow(X_train, Y_train, batch_size=batch_size),
                     validation_data=(X_val, Y_val),
