25780dcada46293298c6eac17842fe156f056176
nsingla <nsingla@thesummitgrp.com>
2024-09-17 12:25:15 +0530

    au folder added

--- xception_test.py
+++ xception_test.py
@@ -151,10 +151,38 @@ image_path = 'Untitled design-2.png'  # Replace with your image path
 # result = predict_image(model, image_path, image_size=(128, 128))
 # print(f"The image {image_path} is predicted to be: {result}")
 
-processed_image = prepare_image_for_prediction(image_path)
-prediction = model.predict(processed_image)
-print(prediction)
-predicted_class = np.argmax(prediction, axis=1)[0]
-class_labels = {0: "Authentic", 1: "Forged"}
-predicted_label = class_labels[predicted_class]
-print(f"Image: {image_path} | Predicted: {predicted_label}")
+
+print("now au folder images")
+print(" ")
+print(" ")
+au_image_folder = "/home/ubuntu/forgery/forgery/data/input/casia-dataset/CASIA2/Au"
+
+# Get the first 5 images from the "Au" folder
+# image_files = os.listdir(au_folder_path)
+# image_files = [f for f in image_files if f.endswith(('.png', '.jpg', '.jpeg'))][:500]  # Pick 5 images
+
+count = 0
+
+
+# Get a list of 5 sample images from the folder
+image_paths = [os.path.join(au_image_folder, image_name) 
+               for image_name in os.listdir(au_image_folder)[:10]]
+
+# Iterate over the sample images, preprocess, and predict
+for image_path in image_paths:
+    # Preprocess the image for prediction
+    processed_image = prepare_image_for_prediction(image_path)
+    
+    if processed_image is None:
+        print(f"Skipping {image_path} due to preprocessing error.")
+        continue
+
+    # Make a prediction using the loaded model
+    prediction = model.predict(processed_image)
+    print(prediction)
+    # Map prediction to class labels
+    predicted_class = np.argmax(prediction, axis=1)[0]
+    class_labels = {0: "Authentic", 1: "Forged"}
+    predicted_label = class_labels[predicted_class]
+
+    print(f"Image: {image_path} | Predicted: {predicted_label}")
