aa93d9e27f4f6643e62e47ddeec95a002ab117ac
nsingla <nsingla@thesummitgrp.com>
2024-09-18 08:21:45 +0530

    temp augment for casia trained

--- aws_model/temp_augment.py
+++ aws_model/temp_augment.py
@@ -10,7 +10,7 @@ import itertools
 from tqdm import tqdm
 
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
 from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
 from tensorflow.keras.optimizers import RMSprop
@@ -110,20 +110,24 @@ datagen = ImageDataGenerator(
 
 datagen.fit(X_train)
 
-model = Sequential()
+# model = Sequential()
 
-model.add(Conv2D(filters = 32, kernel_size = (3,3),padding = 'valid', 
-                 activation ='relu', input_shape = (128,128,3)))
-model.add(Conv2D(filters = 32, kernel_size = (3,3),padding = 'valid', 
-                 activation ='relu'))
-model.add(MaxPool2D(pool_size=(2,2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(256, activation = "relu"))
-model.add(Dropout(0.5))
-model.add(Dense(2, activation = "softmax"))
+# model.add(Conv2D(filters = 32, kernel_size = (3,3),padding = 'valid', 
+#                  activation ='relu', input_shape = (128,128,3)))
+# model.add(Conv2D(filters = 32, kernel_size = (3,3),padding = 'valid', 
+#                  activation ='relu'))
+# model.add(MaxPool2D(pool_size=(2,2)))
+# model.add(Dropout(0.25))
+# model.add(Flatten())
+# model.add(Dense(256, activation = "relu"))
+# model.add(Dropout(0.5))
+# model.add(Dense(2, activation = "softmax"))
 
-model.summary()
+# model.summary()
+
+# Load the pre-trained model
+model = load_model('aws_model_4_casia.h5')
+
 
 optimizer = RMSprop(learning_rate=0.0005, rho=0.9, epsilon=1e-08, decay=0.0)
 model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
@@ -141,5 +145,5 @@ history = model.fit(datagen.flow(X_train, Y_train, batch_size=batch_size),
                     epochs=epochs, verbose=2, callbacks=[early_stopping])
 
 print("starting to save the model")
-model.save("aws_model_4_augmented.h5")
+model.save("aws_model_4_casia_augmented.h5")
 print("ending after save the model")
